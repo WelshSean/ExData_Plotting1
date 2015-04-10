9a7e4550567a91a9ee0c3b8cd7a5252126bcdb51
Sean Clarke <sean@nay-clarke.com>
2015-04-10 23:28:26 +0100

    fixed xlabels

--- figure/plot4.R
+++ figure/plot4.R
@@ -9,11 +9,11 @@ df <- within(df, { timestamp=as.POSIXct(strptime(paste(Date, Time), "%d/%m/%Y %H
 png("plot4.png", width=480, height=480)
 par(mfrow = c(2, 2))   # 2x2 plot
 with(df, {plot(timestamp, Global_active_power, type="l", ylab="Global Active Power", xlab="")
-          plot(timestamp, Voltage, type="l", ylab="Voltage")
+          plot(timestamp, Voltage, type="l", ylab="Voltage", xlab="datetime")
           plot(timestamp, Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
           points(timestamp, Sub_metering_2, type="l", col="red")
           points(timestamp, Sub_metering_3, type="l", col="blue")
           legend("topright", cex=0.9, lty=c(1,1), col = c("black","blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty="n")
-          plot(timestamp, Global_reactive_power, type="l")
+          plot(timestamp, Global_reactive_power, type="l", xlab="datetime")
           })
 dev.off()
